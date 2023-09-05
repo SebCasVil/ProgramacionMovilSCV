809e8a10920f76d4a10da63226490b9a7b2be105
SebCasVil <sebastiancastvill@gmail.com>
2023-09-04 20:37:31 -0600

    Iconos

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,  Dimensions, Image, TextInput } from 'react-native';
-import Login from './src/screens/Login';
+import { StyleSheet, Text, View, StatusBar as ReactStatus, Platform} from 'react-native';
+import Constants from 'expo-constants';
+import { Ionicons } from '@expo/vector-icons';
 
 
 // Exportado por defecto (Unicamente se importa esta función cuando se intente importar en otro archivo)
 export default function App() {
-const{height, width} = Dimensions.get('screen')
-
   return (
     <View style={styles.container}>
-      <Login/>
+      <Text>TEXTO</Text>
+      <Ionicons name='notifications' size={20}></Ionicons>
     </View>
   );
 }
@@ -18,23 +18,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#b2b2ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+
+    // paddingTop: Platform.OS === 'android' ? ReactStatus.currentHeight else auto
+    paddingTop: Constants.statusBarHeight
+    // paddingTop: ReactStatus.currentHeight,
+    // Para tener el mismo efecto pero en iOS podemos usar SafeAreaView
   },
-  text:{
-    color: 'aliceblue',
-  },
-  button:{
-    borderWeight: 3,
-    padding: 5
-  },
-  input:{
-    borderWidth: 1,
-    width: 200,
-    height: 50,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    color: 'red',
-    backgroundColor: 'aliceblue'
-  }
 });
