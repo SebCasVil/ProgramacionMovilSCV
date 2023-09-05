eeb4cc7bd59bc73806abebac69f32a2767b59eb2
SebCasVil <sebastiancastvill@gmail.com>
2023-09-05 17:35:45 -0600

    Base - Primera Pantalla

---
+++ FirstProject/src/components/BotonGeneral/index.jsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const BotonGeneral = ({ icon, size, color, backgroundColor, circleSize}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <View style={[styles.iconContainer, { backgroundColor, width:circleSize, height:circleSize, borderRadius:circleSize/2 }]}>
+          <Ionicons name={icon} size={size} color={color} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: 50
+  },
+  iconContainer: {
+    width: 55, 
+    height: 55, 
+    borderRadius: 27.5, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+});
+
+export default BotonGeneral;
--- FirstProject/src/components/First/FirstHeader.jsx
+++ FirstProject/src/components/First/FirstHeader.jsx
@@ -1,38 +1,60 @@
-import { View, StyleSheet, Text} from "react-native"
+import React from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-export const FirstHeader = () =>{
-    return(
-        <View style={styles.content}>
-            <View style={styles.uno}>
-                <Text style={{fontSize: 30, fontWeight: 'bold', color: '#FFEDDF'}}>Wallet</Text>
-                <View style={{flexDirection: 'row', gap: 10}}>
-                    <Ionicons name="notifications" size={24} color="black" />
-                    <Ionicons name="person" size={24} color="black" />
-                </View>
-            </View>
-            <Text style={{fontSize: 50, fontWeight: 'bold', color: '#FFEDDF'}}>$23,867</Text>
-            <View style={{flexDirection: 'row', gap: 10, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Wallet id: 2678900085</Text>
-                <Ionicons name="copy-outline" size={24} color="black" />
-            </View>
+import BotonGeneral from '../BotonGeneral';
+
+export const FirstHeader = () => {
+  return (
+    <View style={styles.content}>
+      <View style={styles.uno}>
+        <Text style={{ fontSize: 25, fontWeight: 'bold', color: '#FFF8CD' }}>Wallet</Text>
+        <View style={styles.buttonContainer}>
+          <BotonGeneral icon={"notifications"} size={20} backgroundColor={'#FFDCB6'} color={'black'} circleSize={45} />
+          <BotonGeneral icon={"person"} size={20} backgroundColor={'#FFDCB6'} color={'black'} circleSize={45} />
         </View>
-    )
-}
+      </View>
+      <Text style={{ fontSize: 50, fontWeight: 'bold', color: '#FFF8CD' }}>$23,867</Text>
+      <TouchableOpacity style={{ flexDirection: 'row', gap: 5, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ color: '#FFF8CD' }}>Wallet id: 2678900085</Text>
+        <Ionicons name="copy-outline" size={15} color='#FFF8CD' />
+      </TouchableOpacity>
+      <View style={styles.buttonContainer2}>
+        <BotonGeneral icon={"arrow-back"} size={30} backgroundColor={'#FFCB63'} color={'black'} circleSize={75} />
+        <BotonGeneral icon={"arrow-forward"} size={30} backgroundColor={'#FFCB63'} color={'black'} circleSize={75} />
+        <BotonGeneral icon={"repeat"} size={30} backgroundColor={'#FFCB63'} color={'black'} circleSize={75} />
+        <BotonGeneral icon={"add"} size={30} backgroundColor={'#FFCB63'} color={'black'} circleSize={75} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    content: {
-        flex: 0.4,
-        backgroundColor: '#E76500',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    },
-    uno:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 20,
-        width: '100%'
-
-    }
-  });
+  content: {
+    flex: 0.4,
+    backgroundColor: '#E1651B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  uno: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    width: '100%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    gap: 10
+  },
+  buttonContainer2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 20,
+    width: '100%',
+    paddingHorizontal: 15
+  },
+});
