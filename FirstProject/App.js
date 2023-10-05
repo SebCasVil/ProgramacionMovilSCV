0a61e97de7ab25be486510d5a55ce3387bffdd5b
SebCasVil <sebastiancastvill@gmail.com>
2023-10-04 20:04:19 -0600

    Unidad2 Practica9 - Context

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,33 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
-import { StyledText } from './src/components/StyledText';
+import { StyleSheet, View } from 'react-native';
+import { Card } from './src/components/Card';
+import { AppContextProvider } from './src/context/AppContext';
 
-
-// Exportado por defecto (Unicamente se importa esta función cuando se intente importar en otro archivo)
 export default function App() {
+
+
   return (
-    <View style={styles.container /*{backgroundColor:'red', flex: 1, alignItems: 'center', justifyContent: 'center'}*/}>
-      <TouchableHighlight style={styles.button} onPress={()=> console.log('Press')}>
-        <Text style={styles.text}>HELLO WORLD!</Text>
-      </TouchableHighlight>
-      <StyledText text='Prueba de texto' fontSize={40} color='aliceblue' fontWeight='bold'></StyledText>
-      <StatusBar style="light" />
-    </View>
+    <AppContextProvider>
+      <View style={styles.container}>
+        <Card/>
+        <StatusBar style="auto" />
+      </View>
+    </AppContextProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b2b2ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-    color: 'aliceblue',
-  },
-  button:{
-    borderWeight: 3,
-    padding: 5
-  }
 });
